645dfe264289da9d12173b818b18888b4c3ff5d9
Omega-dev0 <47953064+Omega-dev0@users.noreply.github.com>
2025-08-11 06:15:46 +0200

    readme + cleanup

--- src/Main/Server.server.lua
+++ src/Main/Server.server.lua
@@ -13,18 +13,9 @@ mainModule.checkCompatibility(config.configVersion)
 local wrapper = mainModule.wrapper
 print(`USING OMEGA'S TERMINAL v{mainModule.version}`)
 
---━━━━━━━━━━━━━━━━ TESTING ━━━━━━━━━━━━━━━━--
-local testing = true
-
 --━━━━━━━━━━━━━━━━━━━ SERVER ━━━━━━━━━━━━━━━━━━━--
 
 wrapper.Init()
-if testing then
-	print(wrapper)
-	wrapper.config.attackers.team = game.Teams.Attackers
-	wrapper.config.defenders.team = game.Teams.Defenders
-	script.Parent.Parent.Terminals.Default.Configuration["Terminal volume"].Value = workspace.PointA
-end
 
 wrapper:LoadTerminal(script.Parent.Parent.Terminals.Default)
 
@@ -32,7 +23,3 @@ wrapper:AddAddon(wrapper.defaultAddons.Panel)
 wrapper:AddAddon(wrapper.defaultAddons.UI)
 wrapper:AddAddon(wrapper.defaultAddons.StartSequence)
 wrapper:AddAddon(wrapper.defaultAddons.EndSequence)
-
-if testing then
-	print(wrapper)
-end
